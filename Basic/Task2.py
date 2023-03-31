19bbca624cea846e78e7b2829aa4602bc12d52df
babychrita <babychrita@gmail.com>
2023-03-31 21:05:39 +0300

    Greeting message

---
+++ Basic/Task2.py
@@ -0,0 +1,9 @@
+"""Manipulate  strings.
+
+Save your  first and last name as separate variables, then use string concatenation  to add them together
+with a white space in between and print a greeting."""
+
+first_name = "Marharyta"
+last_name = "Babych"
+s = first_name + " " + last_name
+print("Hi! My name is " + str(s) + ". I\'m glad to see you.")
