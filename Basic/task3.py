6699c4721f5d3e0ef48e1c2c1713783f89e231b7
babychrita <babychrita@gmail.com>
2023-04-01 10:03:50 +0300

    Primitive calculator

---
+++ Basic/task3.py
@@ -0,0 +1,21 @@
+"""Using python as a calculator.
+
+Make a program with 2 numbers saved in separate variables a and b, then print the result for each of the following:
+
+Addition
+Subtraction
+Division
+Multiplication
+Exponent (Power)
+Modulus
+Floor division"""
+f = 4
+j = 5
+ad = f+j
+sub = f-j
+div = f/j
+mul = f*j
+ex = f**j
+mod = f%j
+fl = f//j
+print(ad, sub, div, mul, ex, mod, fl, sep='\n' )
